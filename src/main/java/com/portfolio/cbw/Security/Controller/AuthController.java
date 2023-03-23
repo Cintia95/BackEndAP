35cf1ca97428acf49f840c795afa6849dbc325c0
Cintia95 <cintiabw1995@gmail.com>
2023-03-22 23:03:27 -0300

    pagina front agregada

--- src/main/java/com/portfolio/cbw/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/cbw/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://cbwfrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://cbwfrontend.firebaseapp.com/","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
